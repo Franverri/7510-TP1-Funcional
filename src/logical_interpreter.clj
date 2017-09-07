8e2cc42e2f6bac57e7a99cc4f2a5796c5c15ee3a
Franco <franverri@hotmail.com>
2017-09-07 12:26:36 -0300

    Limpieza de codigo

--- src/logical_interpreter.clj
+++ src/logical_interpreter.clj
@@ -3,28 +3,28 @@
 		  	  [clojure.java.io :as io])
 )
 
-(defn tieneParentesis1
-	"Explicar"
+(defn tieneParentesisFinal
+	"Verifica si la linea posee parentesis de cierre"
 	[x]
 	(if (boolean (re-find (re-pattern "\\)") x))  true false)
 )
 
-(defn tieneParentesis2
-	"Explicar"
+(defn tieneParentesisInicial
+	"Verifica si la linea posee parentesis de inicio"
 	[x]
 	(if (boolean (re-find (re-pattern "\\(") x))  true false)
 )
 
 (defn tienePunto
-	"Explicar"
+	"Verifica si la linea posee el punto final"
 	[x]
 	(if (boolean (re-find (re-pattern "\\.") x))  true false)
 )
 
 (defn mapeoSintaxis
-	"Explicar"
+	"Mapea las lineas de entrada con valores de salida del tipo 'bien' si la sintaxis es correcta o 'mal de lo contrario"
 	[x]
-	(if (and (tieneParentesis1 x) (and (tieneParentesis2 x) (tienePunto x))) "bien" "mal")
+	(if (and (tieneParentesisFinal x) (and (tieneParentesisInicial x) (tienePunto x))) "bien" "mal")
 )
 
 (defn bddCorrecta?
@@ -36,17 +36,17 @@
 (defn queryCorrecta?
 	"Verifica que la sintaxis de la consulta sea correcta"
 	[consulta]
-	(if (and (tieneParentesis1 consulta) (tieneParentesis2 consulta)) true false)
+	(if (and (tieneParentesisFinal consulta) (tieneParentesisInicial consulta)) true false)
 )
 
 (defn procesarBDD
-	"Convierte la base de datos en una lista cuyos elementos son cada una de las lineas dadas"
+	"Convierte la base de datos en una lista cuyos elementos son cada una de las lineas dadas (eliminando los puntos)"
 	[bdd]
 	(remove empty? (str/split-lines (str/replace  bdd #"[\t.]" "")))
 )
 
-(defn procesarBDD2
-	"Convierte la base de datos en una lista cuyos elementos son cada una de las lineas dadas"
+(defn procesarBDDconPuntos
+	"Convierte la base de datos en una lista cuyos elementos son cada una de las lineas dadas (conservando los puntos)"
 	[bdd]
 	(remove empty? (str/split-lines bdd))
 )
@@ -69,7 +69,6 @@
 (defn mapeoRegla
 	"Convierte la lista recibida de la bdd en una del tipo (Regla args facts)"
 	[x]
-	;(if (= (re-find #":-" x) ":-") (str/replace  x #"[(]" " (") "")
 	(if (= (re-find #":-" x) ":-") x "")
 )
 
@@ -82,83 +81,31 @@
 (defn evaluarFact
 	"Evalua si la definicion se encuentra en la base de datos"
 	[consulta listaBdd]
-	;(println "Es simple")
-	;(println consulta)
 	(if (= (some #{consulta} listaBdd) consulta) true false)
 )
 
-(defn mapeoVariables
-	"Genera el orden de las variables para acomodar los parametros"
-	[x]
-	(str ":" x)
-)
-
 (defn replace-map
-  "given an input string and a hash-map, returns a new string with all
-  keys in map found in input replaced with the value of the key"
+  "Reemplaza los valores del string por aquellos que se pasen dentro de un mapa (La clave es reemplazada por su valor)"
   [s m]
   (clojure.string/replace s
               (re-pattern (apply str (interpose "|" (map #(java.util.regex.Pattern/quote %) (keys m)))))
           m))
 
-(comment
 (defn sustituirVariables 
-	"Intercambia los X, Y, Z por los valores ingresados por el usuario"
-	[factRegla variablesConsulta mapaVariables]
-	(let [primerArg (get mapaVariables :X)
-		  segundoArg (get mapaVariables :Y)
-		  tercerArg (get mapaVariables :Z)]
-	;(println variablesRegla)
-	;(println factRegla)
-	;(println variablesConsulta)
-	;(println primerArg)
-	;(println segundoArg)
-	;(println tercerArg)
-	(replace-map factRegla {"X" primerArg "Y" segundoArg "Z" tercerArg})
-	)
-)
-)
-
-(defn sustituirVariables 
-	"Intercambia las variables por los valores ingresados por el usuario"
+	"Intercambia las variables por los valores ingresados por el usuario en la consulta"
 	[factRegla variablesConsulta variablesRegla]
-
-	(println factRegla)
-	(println variablesConsulta)
-	(println variablesRegla)
-	(println (count variablesRegla))
-	(println (first variablesRegla))
-	(println (first variablesConsulta))
-
-	(if (> (count variablesRegla) 0) (do (sustituirVariables 
-												(str/replace factRegla (first variablesRegla) (first variablesConsulta))
-												(rest variablesConsulta)
-												(rest variablesRegla)
-										)
-									  )
-	factRegla
-	)
+	(if (> (count variablesRegla) 0) (do (sustituirVariables (str/replace factRegla (first variablesRegla) (first variablesConsulta)) (rest variablesConsulta) (rest variablesRegla))) factRegla)
 )
 
 (defn crearFact
-	"Elabora el fact a cumplir a partir de la regla dada"
+	"Elabora el/los fact/s a cumplir a partir de la regla dada"
 	[variablesConsulta factRegla variablesRegla]
-	(let [nombreFact (subs factRegla 0 (str/index-of factRegla " "))
-		  ;mapaVariables (zipmap [:X :Y :Z] (str/split variablesConsulta #" "))
-		  ]
-		;(println ordenVariables)
-		;(println ordenFinal)
-		;(println (first variablesRegla))
-		;(println variablesRegla)
-		;(sustituirVariables factRegla variablesConsulta mapaVariables)
-		(sustituirVariables factRegla variablesConsulta variablesRegla)
-	)
+	(sustituirVariables factRegla variablesConsulta variablesRegla)
 )
 
 (defn encontrarVars
-	"Funcion para lograr el mapeo correcto y encontrar las variables de la regla dada"
+	"Funcion para lograr el mapeo correcto y encontrar las variables de la regla dada. Devuelve los args si encuentra la funcion y sino nil"
 	[reglaCompleta nombreRegla]
-	;(println reglaCompleta)
 	(if (= nombreRegla (subs reglaCompleta 0 (str/index-of reglaCompleta "("))) (subs reglaCompleta (+(str/index-of reglaCompleta "(")1) (str/index-of reglaCompleta ")")) nil)
 )
 
@@ -175,15 +122,7 @@
 	(let [nombreConsulta (subs consulta 0 (str/index-of consulta " "))
 		  variablesConsulta (str/split (subs consulta (+(str/index-of consulta " ")1)) #" ")
 		  listadoFactsRegla (subs (first listaReglas) (+ (str/index-of (first listaReglas) ":") 3))
-		  ordenVariables (strip (subs listadoFactsRegla (str/index-of listadoFactsRegla "(")) ".,()")
-		  ;fact (crearFact variablesConsulta listadoFactsRegla ordenVariables)
-		  variablesRegla (str/split (first (buscarVariables nombreConsulta listaReglas)) #", ")
-		  ]
-	;(println (first listaReglas))
-	;(println nombreConsulta)
-	;(println variablesRegla)
-	;(println variablesConsulta)
-	;(println (str/split (first variablesRegla) #","))
+		  variablesRegla (str/split (first (buscarVariables nombreConsulta listaReglas)) #", ")]
 	(crearFact variablesConsulta listadoFactsRegla variablesRegla)
 	)
 )
@@ -197,17 +136,8 @@
 (defn evaluarVariosFacts
 	"Evalua todos los facts que compone la regla para verificar si se cumple o no"
 	[facts listaBdd]
-	(let [;primerFact (subs facts 0 (+(str/index-of facts "),")1))
-		  ;segundoFact (subs facts (+(str/index-of facts "),")3))
-		  listaFacts (str/split (replace-map facts {"), " ");"}) #";")]
-	;(println "Es compuesta")
-	;(println primerFact)
-	;(println segundoFact)
-	;(println listaFacts)
-	;(println (str (first listaFacts) "."))
-	;(println (rest listaFacts))
+	(let [listaFacts (str/split (replace-map facts {"), " ");"}) #";")]
 	(every? true? (map #(evaluarFact % listaBdd) listaFacts))
-	;(and (evaluarFact primerFact listaBdd) (evaluarFact segundoFact listaBdd))
 	)
 )
 
@@ -215,9 +145,6 @@
 	"Evalua si la regla se encuentra en la base de datos"
 	[consulta listaBdd listaReglas]
 	(let [facts (buscarRegla (strip (str/replace  consulta #"[(]" " (") ".,()") listaReglas)]
-	;(evaluarFact (buscarRegla (strip (str/replace  consulta #"[(]" " (") ".,()") listaReglas) listaBdd)
-	;(println "Es regla")
-	;(println facts)
 	(if (esCompuesto? facts) (evaluarVariosFacts facts listaBdd) (evaluarFact facts listaBdd))
 	)
 )
@@ -234,10 +161,7 @@
 	(let [listaBdd (procesarBDD bdd)
 		  listaTipo (procesarTipos listaBdd)
 		  listaReglas (procesarReglas listaBdd)
-		  listaVerificacion (procesarBDD2 bdd)]
-		;(println (first listaBdd))
-		;(println listaTipo)
-		;(println listaReglas)
+		  listaVerificacion (procesarBDDconPuntos bdd)]
 		(if (queryCorrecta? consulta)
 			(if (bddCorrecta? listaVerificacion) (evaluarConsulta consulta listaTipo listaBdd listaReglas) nil)
 			nil )
